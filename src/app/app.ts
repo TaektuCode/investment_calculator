76082706be70ecdd048405b8df2aa187902db68f
taektu_coding <taektu.coding@gmail.com>
2025-11-21 11:24:06 +0100

    feat(user-input): create component with form markup and styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { UserInput } from './user-input/user-input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, UserInput],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
