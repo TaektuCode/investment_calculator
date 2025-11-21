a3c5e8d188a81cacd545b6b5bcaa9227b61e78c1
taektu_coding <taektu.coding@gmail.com>
2025-11-21 12:54:12 +0100

    feat(app): implement investment calculation logic and event handling
    - Add calculation logic to App component (Smart Component)
    - Configure UserInput to emit data via Output (Lifting State Up)
    - Connect components in template

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,4 +11,34 @@ import { UserInput } from './user-input/user-input';
 })
 export class App {
   protected readonly title = signal('investment_calculator');
+
+  onCalculateInvestmentResults(data: {
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }) {
+    const { initialInvestment, annualInvestment, expectedReturn, duration } =
+      data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    console.log(annualData);
+  }
 }
